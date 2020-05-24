28ff5d2a8da776139f26ca3c73494a16b287944e
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-05-24 16:22:18 +0200

    first commit

---
+++ zad1.ps1
@@ -0,0 +1,9 @@
+﻿$folderName = Read-Host -Prompt 'Input the folder name'
+$number = Read-Host -Prompt 'Input the number of files to create'
+
+New-Item -Path "./" -Name $folderName -ItemType "directory"
+cd .\$folderName
+
+For ($i=1; $i -le $number; $i++) {
+    New-Item -Path . -Name $i".txt" -ItemType "file" -Value "This is a text string."
+    }
---
+++ zad2.ps1
@@ -0,0 +1,9 @@
+﻿$fileName=$args[0]
+$char=$args[1]
+
+$fullName = [string]$fileName + ".txt"
+
+$FileContent = Get-Content $fullName
+$Matches = Select-String -InputObject $FileContent -Pattern "s" -AllMatches
+
+Write-Host "Number of "$char "occurences: "$Matches.Matches.Count
